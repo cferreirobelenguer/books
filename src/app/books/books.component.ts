6867776eab807a31e9c5c67b22e3156d0f3ea639
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-23 14:57:20 +0200

    get books

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ServiceBookService } from './serviceBook/service-book.service';
+import { Book } from './interfaz/datainterfaz';
+
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent {
+
+  public data: Book[] = [];
+  constructor (
+    private serviceBookService : ServiceBookService
+  ) {
+    this.serviceBookService.getJson().subscribe((res:any) =>{
+      //get json data books
+      this.data = res;
+    })
+  }
+}
