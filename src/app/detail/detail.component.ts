9f4504b3593e8cd7505fc1846fc2cf30f82c3aae
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-02 19:21:54 +0200

    valoration

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceBookService } from '../books/serviceBook/service-book.service';
 import { Book } from '../books/interfaz/datainterfaz';
@@ -9,6 +9,7 @@ import { Book } from '../books/interfaz/datainterfaz';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent {
+  @ViewChildren('starElement') starElements!: QueryList<ElementRef>;
   public id: string | null = '';
   public data: Book[] = [];
   public info: Book = {
@@ -51,5 +52,31 @@ export class DetailComponent {
 
   public handleRedirect() {
     this.router.navigate([''])
+  }
+
+  public handleValoration(index:number) {
+    const indexElement = index;
+    const elementsArray = this.starElements.toArray();
+    const valorationContainer= elementsArray[0].nativeElement;
+    const buttonsArray: HTMLButtonElement[] = Array.from(valorationContainer.querySelectorAll('button'));
+    const puntuation = index +1;
+    buttonsArray.forEach((button: HTMLButtonElement, index:number) => {
+      if( index <=indexElement) {
+        const afterElement = button.querySelector('span');
+        if (afterElement) {
+          afterElement.classList.add('star-after'); 
+          afterElement.style.color = '#f1c40f';
+        }
+      } else if (index> indexElement){
+        const afterElement = button.querySelector('span');
+        if (afterElement) {
+          afterElement.classList.add('star-after'); 
+          afterElement.style.color = '#95a5a6';
+        }
+      }
+    })
+    console.log("La puntuación es : " , puntuation)
+    
+  
   }
 }
